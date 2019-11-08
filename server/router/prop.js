9171f2f4319e8d617848c0bafed1e5e35cfc419f
Sam Li <weisen.li@hotmail.com>
2019-11-08 16:07:20 -0500

    add search feature in api

--- server/router/prop.js
+++ server/router/prop.js
@@ -5,10 +5,16 @@ const listingPicUrls = require('../helpers/prop');
 
 const fields = 'sid his addr ptype2 photonumbers pho phosrc ddfID picUrl phomt saletp city prov lp sp lat lng status bdrms bthrms gr zip';
 router.route('/')
-.get((req,res)=>{
-  Prop.find({},'', {limit:20}).sort('-ts').exec((err, props) => {
+.post((req,res)=>{
+  const filters = req.body;
+  let query = {};
+  if (filters.search) {
+    query.addr = new RegExp(filters.search,'i');
+  }
+  Prop.find(query,'', {limit:20}).sort('-ts').exec((err, props) => {
     for(let i in props) {
-      props[i].photos = listingPicUrls(props[i],false);
+      let p = props[i];
+      props[i].photos = listingPicUrls(p,false);
     }
     res.status(200).json(props);
 	});

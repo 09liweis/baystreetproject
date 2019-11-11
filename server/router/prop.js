257556267de476e7843e61798da140cb9f63f8cd
Sam Li <weisen.li@hotmail.com>
2019-11-11 11:15:12 -0500

    initial import city function

--- server/router/prop.js
+++ server/router/prop.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Prop = require('../models/prop');
+const Prov = require('../models/prov');
 const listingPicUrls = require('../helpers/prop');
 
 const fields = 'sid his addr ptype2 photonumbers pho phosrc ddfID picUrl phomt saletp city prov lp sp lat lng status bdrms bthrms gr zip';
@@ -20,7 +21,33 @@ router.route('/')
 	});
 });
 router.route('/importCity').get((req,res)=>{
-  res.status(200).json('done');
+  Prop.find({},'prov city').exec((err,props)=>{
+    let provs = {};
+    for(let i in props) {
+      let prp = props[i];
+      const c = prp.city;
+      const p = prp.prov;
+      if (provs[p]) {
+        if (provs[p].indexOf(c) == -1) {
+          provs[p].push(c);
+        }
+      } else {
+        provs[p] = [c]
+      }
+    }
+    insertProv(provs);
+  })
 });
+
+function insertProv(provs) {
+  const provsAry = Object.keys(provs);
+  let prov = provsAry.pop();
+  if (prov) {
+    insertVal = {
+      prov,
+      cities:provs[prov]
+    };
+  }
+}
 
 module.exports = router;

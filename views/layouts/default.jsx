be2b736a51dfcccd8bd81fc06494ba51b57c6ca2
Sam Li <weisen.li@hotmail.com>
2019-11-12 15:37:53 -0500

    add detail.css,remove main.js

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -10,6 +10,7 @@ class DefaultLayout extends React.Component {
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
           <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
           <link rel="stylesheet" href="/dist/style.css" type="text/css" />
+          <link rel="stylesheet" href="/dist/detail.css" type="text/css" />
           <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.0/mapbox-gl.css' rel='stylesheet' />
           <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.0/mapbox-gl.js'></script>
         </head>
@@ -17,7 +18,6 @@ class DefaultLayout extends React.Component {
           <header>Bay Street Project</header>
           <main>{this.props.children}</main>
           <footer>Footer</footer>
-          <script src="/dist/main.js"/>
         </body>
       </html>
     );

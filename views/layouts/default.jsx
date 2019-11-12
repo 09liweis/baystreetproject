e4f8cfaf58b7a4061ea16401de46cd4d2e391f49
Sam Li <weisen.li@hotmail.com>
2019-11-12 18:05:23 -0500

    style header

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -11,11 +11,18 @@ class DefaultLayout extends React.Component {
           <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
           <link rel="stylesheet" href="/dist/style.css" type="text/css" />
           <link rel="stylesheet" href="/dist/detail.css" type="text/css" />
+          <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"/>
           <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.0/mapbox-gl.css' rel='stylesheet' />
           <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.0/mapbox-gl.js'></script>
         </head>
         <body>
-          <header>Bay Street Project</header>
+          <header id="header">
+            <h2 id="logo">Bay Street</h2>
+            <div id="nav">
+              <a className="navItem" href="/">Buy</a>
+              <a className="navItem" href="/stats">Stats</a>
+            </div>
+          </header>
           <main>{this.props.children}</main>
           <footer>Footer</footer>
         </body>

470597ebb1a25c0287a65a3a52b5857cec9192f1
yingxuesun <syxcxd@gmail.com>
2019-11-15 15:44:20 -0500

    detail introduction

--- views/detail.jsx
+++ views/detail.jsx
@@ -15,34 +15,55 @@ class Detail extends React.Component {
             <span className="detailCity">{prop.city}, </span>
             <span className="detailprov">{prop.prov}, </span>
             <span className="detailzip">{prop.zip} </span>
+          <div className="detailcrossstreet">{prop.crsst}</div>
           <div className="dtitle"></div> 
-          <div className="detaillstd">List Date:{prop.lstd}</div> 
+          <div className="detaillisrdate">List Date:{prop.lstd}</div> 
           </div>
           <div className="detailImgs">
             {imgs}
           </div>
           <div className="">
-          <span className="detaillp">${prop.lp}</span>
-          <span className="detailpunt">{prop.lpunt}</span>
+          <span className="detailprice">${prop.lp}</span>
+          <span className="detailforsale">{prop.lpunt}</span>
           </div>
           <div className="detailunt">{prop.unt}</div>
-          
-          <div className="detailtax">{prop.tax}</div>
-          <div className="detailtaxyr">{prop.taxyr}</div>
-          <div className="detailstorey">{prop.pstyl}</div>
-          <div className="detailtotalparkingspace">{prop.tot_park_spcs}</div>
-          <div className="detailtp">{prop.ltp}</div>
-          
-          
-          <div className="detailCmty">{prop.cmty}</div>
-          <div className="detailbedroom">Bedroom: {prop.bdrms}</div>
-          <div className="detailbathroom">Bathroom: {prop.bthrms}</div>
+
+          <div className="dtitle2"></div> 
           <div className="detailtotalbedrooms">Total Bedrooms: {prop.tbdrms}</div>
-          <div className="detailgarage">Garage: {prop.tgr}</div>
+          <div className="detailbathroom">Bathroom: {prop.bthrms}</div>
+          <div className="detailkitchen">Kitchen: {prop.kch}</div>
+          <div className="detailgarage">Total Garage: {prop.tgr}</div>
+          <div className="detailtotalparkingspace">Total parking space:{prop.tot_park_spcs}</div>
+          
+          <div className="dtitle1"></div> 
+          <div className="detailsqft">Square Feet:{prop.sqft1}</div>
+          <div className="detailtaxyr">Tax Year:{prop.taxyr}</div>
+          <div className="detailtax">Tax: {prop.tax}</div>
+          <div className="dtitle2"></div> 
+
+          
+          
+          
+          
+          
+          <div className="detailstorey">Storey:{prop.pstyl}</div>
           <div className="detailgeoq">Geoq: {prop.geoq}</div>
           <div className="detailwater">Water: {prop.water}</div>
           <div className="detailrealestatecenter">{prop.rltr}</div>
-          <div className="detailcrossstreet">{prop.crsst}</div>
+          <div className="detailfeat">{prop.feat}</div>
+          <div className="detailage">Age:{prop.age}</div>
+          <div className="detailrealtor">Realtor:{prop.rltr}</div>
+          <div className="detailrealtor">Sewer:{prop.sewer}</div>
+          <div className="MLS">MLS#:{prop.sid}</div>
+          <div className="pool">Pool:{prop.pool}</div>
+          <div className="detaillaundrylevel">Laundry level:{prop.laundry_lev}</div>
+          <div className="detailinternet">Internet:{prop.internet}</div>
+          <div className="detailheat">Heat:{prop.heat}</div>
+          <div className="detailac">Air Condition:{prop.ac}</div>
+
+
+          <div className="dtitle2"></div> 
+          <div className="detaildetail">Detail Introduction：{prop.m}</div>
         </div>
       </DefaultLayout>
     )
